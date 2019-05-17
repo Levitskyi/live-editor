0d2a43c02ef384e7d4c14cfcbc85f4fdca5e4746
taraslevitskyi <taraslevitskyi@gmail.com>
2019-05-18 00:41:47 +0300

    Updates

---
+++ precache-manifest.b7625fa0989d7eb6f4bdb92c0cb6a9eb.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6972a394ced41cb4034ed2d55a82e19e",
+    "url": "/live-editor/editor.worker.js"
+  },
+  {
+    "revision": "aa37b32b848290900a42719768a5da5d",
+    "url": "/live-editor/index.html"
+  },
+  {
+    "revision": "30077851586d58c9d73df2ab51971d30",
+    "url": "/live-editor/json.worker.js"
+  },
+  {
+    "revision": "9709f7f969b1029ade31",
+    "url": "/live-editor/static/css/2.b7884e10.chunk.css"
+  },
+  {
+    "revision": "81c62df1215948bfa5c6",
+    "url": "/live-editor/static/css/main.031250c4.chunk.css"
+  },
+  {
+    "revision": "9709f7f969b1029ade31",
+    "url": "/live-editor/static/js/2.3ba6995f.chunk.js"
+  },
+  {
+    "revision": "7b0569c0c68b02b9d4c0",
+    "url": "/live-editor/static/js/3.c6932990.chunk.js"
+  },
+  {
+    "revision": "edbd60a2b71c44617703",
+    "url": "/live-editor/static/js/4.5f0f7392.chunk.js"
+  },
+  {
+    "revision": "89e463fbf4fba261f84f",
+    "url": "/live-editor/static/js/5.0fc3f435.chunk.js"
+  },
+  {
+    "revision": "b7bc948faa0b134c4d8c",
+    "url": "/live-editor/static/js/6.ca06610b.chunk.js"
+  },
+  {
+    "revision": "81c62df1215948bfa5c6",
+    "url": "/live-editor/static/js/main.e5218416.chunk.js"
+  },
+  {
+    "revision": "e5b3299724373133b10f",
+    "url": "/live-editor/static/js/runtime~main.ba7687b3.js"
+  },
+  {
+    "revision": "63882674c98de49e72bb1a6ec0ab0db6",
+    "url": "/live-editor/typescript.worker.js"
+  }
+]);
